cf24f7f6643bedb21f3496f982ff36abdd3032f7
Edd2828 <edwardapplication13@gmail.com>
2025-07-19 15:01:05 +0100

    rename config

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from utils.get_data import (
     )
 
 
-etls = [
+etls_config = [
     {'name': 'countries', 'class': GetCountries, 'sort_data': 'common_name', 'file_name': 'countries', 'filters': ['name', 'population']},
     {'name': 'country_currency', 'class': GetCountryCurrency,'sort_data': 'country_name', 'file_name': 'country_currency', 'filters': ['name', 'currencies']},
     {'name': 'country_language', 'class': GetCountryLanguage,'sort_data': 'country_name', 'file_name': 'country_language', 'filters': ['name', 'languages']},
@@ -29,7 +29,7 @@ currency_extractor = GetCountryCurrency(sort_data='country_name', file_name='cou
 
 # Execute in parallel
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    futures = [executor.submit(run_extraction, create_extractor(etl['class'], etl['sort_data'], etl['file_name'], etl['filters'])) for etl in etls]
+    futures = [executor.submit(run_extraction, create_extractor(etl['class'], etl['sort_data'], etl['file_name'], etl['filters'])) for etl in etls_config]
     
     # Wait for all futures to complete
     for future in concurrent.futures.as_completed(futures):
