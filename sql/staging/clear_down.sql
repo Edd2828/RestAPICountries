9bedbbba634bf0a74a7f1b970af9482a9272f012
Edd2828 <edwardapplication13@gmail.com>
2025-07-20 13:07:29 +0100

    Add Report

--- sql/staging/clear_down.sql
+++ sql/staging/clear_down.sql
@@ -3,5 +3,3 @@ drop table if exists staging.country_currency;
 drop table if exists staging.languages;
 drop table if exists staging.currencies;
 drop table if exists staging.country_language;
-
-drop schema if exists staging;
